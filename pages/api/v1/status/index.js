a71dd7a07632fa3651f5472eb8100350a34c91cb
Arthur Bassi <arthur.winiarski.ranger@outlook.com>
2024-02-17 19:00:29 -0400

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,10 @@
-// touter name /api/status
+// router name /api/status
+import database from "../../../../infra/database.js";
 
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+
+  console.log(result.rows);
   response.status(200).json({ chave: "valor" });
 }
 
